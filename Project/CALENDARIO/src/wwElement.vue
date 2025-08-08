cc294e444684b18902cdd8773493638c5276180c
thiagocantao <thiago.cantao@icloud.com>
2025-08-08 11:49:02 -0300

    Update wwElement.vue

--- Project/CALENDARIO/src/wwElement.vue
+++ Project/CALENDARIO/src/wwElement.vue
@@ -11,7 +11,7 @@
         <tbody>
           <tr v-for="day in weekDays" :key="day.name">
             <td>
-              <input type="checkbox" v-model="day.active" />
+              <input type="checkbox" style="cursor:pointer" v-model="day.active" />
               {{ day.label }}
             </td>            
             <td>
@@ -71,16 +71,22 @@
     </div>
 
     <div class="excluded-dates">
-      <h3>Datas excluídas</h3>
+      <h3>Excluir datas</h3>
       <table>
+      <thead>
+          <tr>
+            <th class="headerDias">Data</th>
+            <th class="headerHoras">Ação</th>
+          </tr>
+        </thead>
         <tbody>
           <tr>
-            <td><input type="date" v-model="newExcludedDate" /></td>
-            <td><button @click="addExcludedDate">Inserir</button></td>
+            <td><input class="inputDate" type="date" v-model="newExcludedDate" /></td>
+            <td><button class="buttonFormat" @click="addExcludedDate">Adicionar</button></td>
           </tr>
           <tr v-for="date in excludedDates" :key="date.toISOString()">
             <td>{{ formatDate(date) }}</td>
-            <td><button @click="removeExcluded(date)">Excluir</button></td>
+            <td><button class="buttonFormat" @click="removeExcluded(date)">Excluir</button></td>
           </tr>
         </tbody>
       </table>
@@ -103,7 +109,7 @@ export default {
     const weekDays = ref([
       {
         name: "mon",
-        label: "Segunda",
+        label: "Segunda-feira",
         active: false,
         shift1Start: "",
         shift1End: "",
@@ -112,7 +118,7 @@ export default {
       },
       {
         name: "tue",
-        label: "Terça",
+        label: "Terça-feira",
         active: false,
         shift1Start: "",
         shift1End: "",
@@ -121,7 +127,7 @@ export default {
       },
       {
         name: "wed",
-        label: "Quarta",
+        label: "Quarta-feira",
         active: false,
         shift1Start: "",
         shift1End: "",
@@ -130,7 +136,7 @@ export default {
       },
       {
         name: "thu",
-        label: "Quinta",
+        label: "Quinta-feira",
         active: false,
         shift1Start: "",
         shift1End: "",
@@ -139,7 +145,7 @@ export default {
       },
       {
         name: "fri",
-        label: "Sexta",
+        label: "Sexta-feira",
         active: false,
         shift1Start: "",
         shift1End: "",
@@ -219,24 +225,47 @@ export default {
 
 .corporate-calendar {
   font-family: "Roboto", sans-serif;
-  font-size: 12px;
+  font-size: 13px;
   color: #333;
   font-weight: 400;
   max-width: 450px;
   
 }
 
-.shift-table,
-.excluded-dates table {
+.buttonFormat{
+  padding:5px;
+  width:80px;
+  color: #ffffff;
+  background-color: #689d8c;
+  border-radius: 15px;
+  cursor:pointer;
+}
+
+.shift-table {
   width: 100%;
   border-collapse: collapse;
   border: 1px solid #acacad;
+}
+
+.excluded-dates table {
+  width: 300px;
+  border-collapse: collapse;
+  border: 1px solid #acacad;
+}
+
+.inputDate{
+padding:3px;
+font-family: "Roboto", sans-serif;
+  font-size: 13px;
+  color: #333;
+  cursor: pointer;
 }
 
 .excluded-dates td {
   border: 1px solid #acacad;
   padding: 4px;
   text-align: center;
+  height:38px;
 }
 
 .shift-table td {
@@ -249,11 +278,12 @@ text-align: center;
 .shift-table select {
   width: 90px;
   font-family: "Roboto", sans-serif;
-  font-size: 12px;
+  font-size: 13px;
   color: #333;
   padding:2px;
   border: 1px solid #acacad !important;
   border-radius: 4px;
+  cursor: pointer;
 }
 
 .shift-table select:focus {
@@ -273,13 +303,21 @@ text-align: center;
   column-gap: 8px;
 }
 
-.corporate-calendar th,
-.corporate-calendar h3 {
+.corporate-calendar th {
   font-weight: 400;
 }
 
+.corporate-calendar h3 {
+  font-weight: 400;  
+  margin-bottom: 1px;
+  margin-top: 25px;
+  font-family: "Roboto", sans-serif;
+  font-size: 13px;
+  color: #333;
+}
+
 .excluded-dates {
-  margin-top: 16px;
+  margin-top: 0px;
 }
 
 .headerDias {
