526db640a7c2a792db517a98733d0f04e3abedf0
thiagocantao <thiago.cantao@icloud.com>
2025-08-20 11:06:02 -0300

    Update ww-config.js

--- Project/InputMentions/ww-config.js
+++ Project/InputMentions/ww-config.js
@@ -63,6 +63,7 @@ export default {
                 'mentionList',
                 'mentionIdPath',
                 'mentionLabelPath',
+                'mentionHintPath'
             ],
             [
                 'parameterTitle',
