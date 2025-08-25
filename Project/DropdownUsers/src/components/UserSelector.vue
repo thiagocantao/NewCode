b7168e9c65bca043d10818139e4bab02776c64f4
thiagocantao <thiago.cantao@icloud.com>
2025-08-25 17:46:05 -0300

    fix: honor empty initial ids

--- Project/DropdownUsers/src/components/UserSelector.vue
+++ Project/DropdownUsers/src/components/UserSelector.vue
@@ -271,6 +271,7 @@ export default {
     unassignedLabel: { type: String, default: 'Unassigned' },
     searchPlaceholder: { type: String, default: 'Search user...' },
     initialSelectedId: [String, Number, Object],
+    initialGroupId: [String, Number],
     selectedUserId: [String, Number, Object],
     uid: String,
     maxWidth: [String, Number],
@@ -383,13 +384,15 @@ export default {
         this.setSelectedFromValue(newVal);
       }
     },
-    initialSelectedId(newVal) {
-      this.setSelectedFromValue(newVal);
+    initialSelectedId() {
+      this.initializeSelectedUser();
+    },
+    initialGroupId() {
+      this.initializeSelectedUser();
     },
     datasource: {
       handler() {
-        const target = this.selectedUserId || this.initialSelectedId;
-        this.setSelectedFromValue(target);
+        this.initializeSelectedUser();
       },
       deep: true
     },
@@ -472,7 +475,20 @@ export default {
       return ['GROUP', 'GROUPS', 'GRUPO', 'GRUPOS'].includes(value);
     },
     initializeSelectedUser() {
-      const target = this.selectedUserId || this.initialSelectedId;
+      let target = this.selectedUserId;
+      const hasSelected = target !== undefined && target !== null && target !== '';
+      if (!hasSelected) {
+        const groupId =
+          this.initialGroupId !== undefined && this.initialGroupId !== null && this.initialGroupId !== ''
+            ? this.initialGroupId
+            : null;
+        const userId =
+          this.initialSelectedId !== undefined && this.initialSelectedId !== null && this.initialSelectedId !== ''
+            ? this.initialSelectedId
+            : null;
+
+        target = groupId !== null ? { userid: userId, groupid: groupId } : userId;
+      }
       this.setSelectedFromValue(target);
     },
     setSelectedFromValue(value) {
@@ -482,20 +498,27 @@ export default {
         return;
       }
       if (typeof value === 'object') {
-        const group = value.groupid != null ? (this.datasource || []).find(u => String(u.id) === String(value.groupid)) : null;
+        const hasGroupId = value.groupid !== undefined && value.groupid !== null && value.groupid !== '';
+        const group = hasGroupId ? (this.datasource || []).find(u => String(u.id) === String(value.groupid)) : null;
         this.selectedGroup = group || null;
-        if (group && value.userid != null) {
+
+        const hasUserId = value.userid !== undefined && value.userid !== null && value.userid !== '';
+        if (group && hasUserId) {
           const user = (group.groupUsers || []).find(u => String(u.id) === String(value.userid));
           this.selectedUser = user || null;
-        } else if (group && value.userid == null) {
+        } else if (group && !hasUserId) {
           this.selectedUser = null;
-        } else {
+        } else if (hasUserId) {
           const user = (this.datasource || []).find(u => String(u.id) === String(value.userid));
           this.selectedUser = user || null;
           this.selectedGroup = null;
+        } else {
+          this.selectedUser = null;
+          this.selectedGroup = null;
         }
       } else {
-        const user = (this.datasource || []).find(u => String(u.id) === String(value));
+        const hasId = value !== undefined && value !== null && value !== '';
+        const user = hasId ? (this.datasource || []).find(u => String(u.id) === String(value)) : null;
         this.selectedUser = user || null;
         this.selectedGroup = null;
       }
