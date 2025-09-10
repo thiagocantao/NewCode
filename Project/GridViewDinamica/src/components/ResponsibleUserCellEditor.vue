f50ea664fdedad43294aeb225b914c13b4e1bb0e
thiagocantao <thiago.cantao@icloud.com>
2025-09-10 15:55:13 -0300

    Create  ResponsibleUserCellEditor.vue

---
+++ Project/GridViewDinamica/src/components/ResponsibleUserCellEditor.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="responsible-user-editor">
+    <UserSelector
+      ref="selector"
+      :datasource="options"
+      group-by="type"
+      :selected-user-id="params.value"
+      @user-selected="onSelected"
+    />
+  </div>
+</template>
+
+<script>
+import { ref, onMounted, nextTick } from 'vue';
+import UserSelector from './UserSelector.vue';
+
+
+export default {
+  name: 'ResponsibleUserCellEditor',
+  components: { UserSelector },
+  props: {
+    params: { type: Object, required: true }
+  },
+  setup(props) {
+    const options = ref([]);
+    const value = ref(props.params.value || null);
+    const selector = ref(null);
+
+    const loadOptions = async () => {
+      if (props.params.options && props.params.options.length) {
+        options.value = props.params.options;
+        return;
+      }
+      try {
+        const lang = window.wwLib?.wwVariable?.getValue('aa44dc4c-476b-45e9-a094-16687e063342');
+        const companyId = window.wwLib?.wwVariable?.getValue('5d099f04-cd42-41fd-94ad-22d4de368c3a');
+        const apiUrl = window.wwLib?.wwVariable?.getValue('1195995b-34c3-42a5-b436-693f0f4f8825');
+        const apiKey = window.wwLib?.wwVariable?.getValue('d180be98-8926-47a7-b7f1-6375fbb95fa3');
+        const apiAuth = window.wwLib?.wwVariable?.getValue('dfcde09f-42f3-4b5c-b2e8-4314650655db');
+        const fetchOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            ...(companyId ? { p_idcompany: companyId } : {}),
+            ...(lang ? { p_language: lang } : {})
+          })
+        };
+        if (apiKey) fetchOptions.headers['apikey'] = apiKey;
+        if (apiAuth) fetchOptions.headers['Authorization'] = apiAuth;
+        const response = await fetch(apiUrl + 'getLookupGroupsAndUsers', fetchOptions);
+        const data = await response.json();
+        options.value = Array.isArray(data) ? data : [];
+      } catch (e) {
+        options.value = [];
+      }
+    };
+
+    const onSelected = (val) => {
+      value.value = val;
+      if (props.params.api && props.params.api.stopEditing) {
+        props.params.api.stopEditing();
+      } else if (props.params.stopEditing) {
+        props.params.stopEditing();
+      }
+    };
+
+    const getValue = () => value.value;
+    const isPopup = () => true;
+
+    onMounted(async () => {
+      await loadOptions();
+      nextTick(() => {
+        selector.value && selector.value.toggleDropdown && selector.value.toggleDropdown();
+      });
+    });
+
+    return { options, onSelected, getValue, isPopup, selector };
+  }
+};
+</script>
+
+<style scoped>
+.responsible-user-editor {
+  min-width: 220px;
+}
+</style>
