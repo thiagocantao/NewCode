9bcff364c564bda4116ee4aa2efbb96e63dc31a1
thiagocantao <thiago.cantao@icloud.com>
2025-11-26 20:28:30 -0300

    Add icon-based ImageUpload component

---
+++ Project/ImageUpload/ww-config.js
@@ -0,0 +1,87 @@
+export default {
+  options: {
+    lazyHydrate: true,
+    linkable: true,
+  },
+  inherit: [{ type: 'ww-layout' }, { type: 'ww-background-video' }],
+  editor: {
+    label: {
+      en: 'Image upload',
+      pt: 'Envio de imagem',
+    },
+    icon: 'attach_file',
+    bubble: {
+      icon: 'attach_file',
+    },
+  },
+  properties: {
+    imageUrl: {
+      label: {
+        en: 'Image URL',
+        pt: 'URL da imagem',
+      },
+      type: 'Text',
+      bindable: true,
+      defaultValue: '',
+    },
+    imageHeight: {
+      label: {
+        en: 'Image height',
+        pt: 'Altura da imagem',
+      },
+      type: 'Select',
+      bindable: true,
+      defaultValue: 'medium',
+      options: {
+        options: [
+          {
+            label: { en: 'Small (150px)', pt: 'Pequena (150px)' },
+            value: 'small',
+          },
+          {
+            label: { en: 'Medium (200px)', pt: 'Média (200px)' },
+            value: 'medium',
+          },
+          {
+            label: { en: 'Large (300px)', pt: 'Grande (300px)' },
+            value: 'large',
+          },
+        ],
+      },
+    },
+    iconName: {
+      label: {
+        en: 'Icon name (Material Symbols)',
+        pt: 'Nome do ícone (Material Symbols)',
+      },
+      type: 'Text',
+      bindable: true,
+      defaultValue: 'attach_file',
+    },
+  },
+  events: {
+    onUpload: {
+      label: { en: 'On image upload', pt: 'Ao enviar imagem' },
+      event: 'onUpload',
+      value: {
+        type: 'object',
+        label: { en: 'Upload payload' },
+      },
+    },
+  },
+  triggerEvents: [
+    {
+      name: 'onUpload',
+      label: { en: 'On image upload' },
+      event: {
+        value: {
+          bucket: '',
+          storagePath: '',
+          signedUrl: '',
+          file: null,
+        },
+      },
+    },
+  ],
+  actions: [{ label: { en: 'Remount component', pt: 'Remontar componente' }, action: 'remount' }],
+};
