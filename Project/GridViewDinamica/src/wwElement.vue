ab18d157fadfcac2b09fd71038d3db2ac79c4086
thiagocantao <thiago.cantao@icloud.com>
2026-03-10 09:57:14 -0300

    Update wwElement.vue

--- Project/GridViewDinamica/src/wwElement.vue
+++ Project/GridViewDinamica/src/wwElement.vue
@@ -3489,16 +3489,6 @@ setTimeout(() => {
         payload = payload.Sort;
       } else if (Array.isArray(payload.sort)) {
         payload = payload.sort;
-      } else {
-        const hasSingleSortEntryFields =
-          payload?.id != null ||
-          payload?.colId != null ||
-          payload?.field != null ||
-          payload?.fieldId != null;
-
-        if (hasSingleSortEntryFields) {
-          payload = [payload];
-        }
       }
     }
 
