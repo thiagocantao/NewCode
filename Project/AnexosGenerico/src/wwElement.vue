43e3f263fda131bcdc1beff9fd7a9ccd7bbb15bb
thiagocantao <thiago.cantao@icloud.com>
2026-05-08 15:38:15 -0300

    fix: expose isFromInitialValue for new uploads

--- Project/AnexosGenerico/src/wwElement.vue
+++ Project/AnexosGenerico/src/wwElement.vue
@@ -425,27 +425,7 @@ export default {
 
         const localData = ref({
             fileUpload: {
-                value: computed(() => {
-                    return fileList.value.map(file => {
-                        const plainObject = {};
-                        for (const key in file) {
-                            if (Object.prototype.hasOwnProperty.call(file, key)) {
-                                plainObject[key] = file[key];
-                            }
-                        }
-                        plainObject.name = file.name;
-                        plainObject.size = file.size;
-                        plainObject.type = file.type;
-                        plainObject.lastModified = file.lastModified;
-                        plainObject.mimeType = file.mimeType;
-                        plainObject.id = file.id;
-
-                        if (file.base64) plainObject.base64 = file.base64;
-                        if (file.binary) plainObject.binary = file.binary;
-
-                        return plainObject;
-                    });
-                }),
+                value: computed(() => fileList.value),
                 status: status,
                 deletedFile: deletedFile,
                 deletedFilesCount: deletedFilesCount,
@@ -600,6 +580,7 @@ export default {
                 if (validationResult.valid) {
                     file.id = `file-${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
                     file.mimeType = file.type;
+                    file.isFromInitialValue = false;
                     if (exposeBase64.value) file.base64 = await fileToBase64(file);
                     if (exposeBinary.value) file.binary = await fileToBinary(file);
                     processedFiles.push(file);
